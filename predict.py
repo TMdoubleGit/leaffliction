3f6960ab2286146691b15f03bc5582bd81964b18
tmichel- <tmichel-@student.42.com>
2025-02-13 08:28:25 +0100

    using class_names.pkl

--- predict.py
+++ predict.py
@@ -35,10 +35,10 @@ def predict_image(image_path):
     predictions = []
 
     model = load_model('./saved_model/leafflication.keras')
-    # with open("./saved_model/classes_names.pkl", "rb") as fichier:
-    #     classes_names = pickle.load(fichier)
+    with open("./saved_model/classes_names.pkl", "rb") as fichier:
+        classes_names = pickle.load(fichier)
 
-    classes_names = ['Apple_Black_rot', 'Apple_healthy', 'Apple_rust', 'Apple_scab', 'Grape_Black_rot', 'Grape_Esca', 'Grape_healthy', 'Grape_spot']
+    # classes_names = ['Apple_Black_rot', 'Apple_healthy', 'Apple_rust', 'Apple_scab', 'Grape_Black_rot', 'Grape_Esca', 'Grape_healthy', 'Grape_spot']
     transformed_images = apply_transformations_to_image(image_path,
                                                         save_dir=None,
                                                         transformations={"blur", "mask", "roi", "analyze", "pseudolandmarks"},
@@ -68,7 +68,7 @@ def predict_image(image_path):
 
 def predict(image_path):
     predicted_class, transformed_images = predict_image(image_path)
-    display_prediction(transformed_images, predicted_class)
+    # display_prediction(transformed_images, predicted_class)
     
 
 if __name__ == "__main__":
